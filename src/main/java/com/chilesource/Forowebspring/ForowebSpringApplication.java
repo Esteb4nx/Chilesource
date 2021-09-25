e7ea15ebddb8dc44926de41d0761cfe81abebf2a
Esteban Esparza <e.esparza02@ufromail.cl>
2021-09-25 14:45:03 -0300

    First Commit
    Initial proyect

---
+++ src/main/java/com/chilesource/Forowebspring/ForowebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.chilesource.Forowebspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForowebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForowebSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chilesource/Forowebspring/ForowebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chilesource.Forowebspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForowebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
