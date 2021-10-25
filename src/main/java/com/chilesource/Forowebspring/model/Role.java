299443b6c1b6e04344aa0a4460cb43acce1e7d81
jmainhard <63559099+jmainhard@users.noreply.github.com>
2021-10-25 00:36:23 -0300

    Añadidos modelos principales para la demostración

---
+++ src/main/java/com/chilesource/Forowebspring/model/Role.java
@@ -0,0 +1,44 @@
+/*
+ *  @author Jorge M.
+ *  25-10-2021
+ */
+
+package com.chilesource.Forowebspring.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int id;
+    @Column(name = "role_name")
+    private String name;
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role() {
+
+    }
+
+    // Getter y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/chilesource/Forowebspring/model/Session.java
@@ -0,0 +1,44 @@
+/*
+ *  @author Jorge M.
+ *  25-10-2021
+ */
+
+package com.chilesource.Forowebspring.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "session_id")
+    private int sessionId;
+    @Column(name = "user_id")
+    private int userId;
+
+    public Session(int sessionId, int userId) {
+        this.sessionId = sessionId;
+        this.userId = userId;
+    }
+
+    public Session() {
+
+    }
+
+    // Getters y setters
+    public int getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(int sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
