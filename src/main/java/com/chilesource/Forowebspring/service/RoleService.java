45449137be01961520da77ff4642212e2c049517
jmainhard <63559099+jmainhard@users.noreply.github.com>
2021-10-25 00:40:30 -0300

    Typo

--- src/main/java/com/chilesource/Forowebspring/service/RoleService.java
+++ src/main/java/com/chilesource/Forowebspring/service/RoleService.java
@@ -7,12 +7,13 @@ package com.chilesource.Forowebspring.service;
 
 import com.chilesource.Forowebspring.commons.GenericService;
 import com.chilesource.Forowebspring.model.Role;
+import com.chilesource.Forowebspring.repository.RoleRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService extends GenericService<Role, Integer> {
-    public RoleService(CrudRepository<Role, Integer> repository) {
+    public RoleService(RoleRepository repository) {
         super(repository);
     }
 }
