505eba87aef57489210320ad62dec807f51fa517
jmainhard <63559099+jmainhard@users.noreply.github.com>
2021-10-27 05:16:37 -0300

    Obteniendo categorías de la base de datos para new-post

--- src/main/java/com/chilesource/Forowebspring/controllers/MainController.java
+++ src/main/java/com/chilesource/Forowebspring/controllers/MainController.java
@@ -1,36 +1,24 @@
 package com.chilesource.Forowebspring.controllers;
 
-import com.chilesource.Forowebspring.commons.GenericService;
-import com.chilesource.Forowebspring.model.Category;
 import com.chilesource.Forowebspring.service.CategoryService;
+import com.chilesource.Forowebspring.service.PostService;
 import com.chilesource.Forowebspring.service.SuperCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    // Autowired servía aquí para no tener que asignarle un constructor al controlador
-    // o eso entiendo: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/beans/factory/annotation/Autowired.html
-    // de cualquier forma los lo saqué por que la IDE me recomendó no agregarlo
-    // luego lo agregué para no tener que hacer mas controller classes
     @Autowired
     private CategoryService categoryService;
 
     @Autowired
     private SuperCategoryService superCategoryService;
 
-    // Este constructor serviria en cada controlador, finalmente dejé la etiqueta Autowired en su lugar
-    // ya que dos atributos me dejó de compilar por lo que probablemente haya que hacer más de un controlador
-    // con esta práctica, por una parte se modulariza todo más y queda más claro lo dejo a tu criterio
-//    public MainController(/* SuperCategoryService supCategoryService */ CategoryService service) {
-//        this.categoryService = categoryService;
-//        this.supCategoryService = supCategoryService;
-//    }
+    @Autowired
+    private PostService postService;
 
     @RequestMapping("/")
     public String index(Model model) {
@@ -38,10 +26,8 @@ public class MainController {
         model.addAttribute("superCategories", superCategoryService.findAll());
         model.addAttribute("categories", categoryService.findAll());
 
-
         return "index";
     }
-
 
     @RequestMapping("/favorites")
     public String favorites() {
--- src/main/java/com/chilesource/Forowebspring/controllers/NewPostController.java
+++ src/main/java/com/chilesource/Forowebspring/controllers/NewPostController.java
@@ -5,7 +5,6 @@
 
 package com.chilesource.Forowebspring.controllers;
 
-import com.chilesource.Forowebspring.model.Article;
 import com.chilesource.Forowebspring.model.Post;
 import com.chilesource.Forowebspring.service.CategoryService;
 import com.chilesource.Forowebspring.service.PostService;
@@ -13,8 +12,6 @@ import com.chilesource.Forowebspring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,26 +35,14 @@ public class NewPostController {
     @GetMapping
     public String main(Model model) {
         model.addAttribute("post", new Post());
+        model.addAttribute("categories", categoryService.findAll());
         return "new-post";
     }
 
-//    @PostMapping
-//    public String savePost(Post post, Model model) {
-//        post.setDate(new Date());
-//        post.setAuthor(userService.findById(1));
-//        post.setCategory(categoryService.findById(1));
-//        postService.save(post);
-//        return "redirect:/post?id=" + post.getId();
-//    }
-
-    @PostMapping("/addpost")
-    public String save(@ModelAttribute Post post, BindingResult result) {
-        if (result.hasErrors()) {
-            return "new-post";
-        }
+    @PostMapping
+    public String save(@ModelAttribute Post post) {
         post.setDate(new Date());
         post.setAuthor(userService.findById(1));
-        post.setCategory(categoryService.findById(1));
         postService.save(post);
         return "redirect:/post?id=" + post.getId();
     }
